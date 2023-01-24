c58a129702d8c7104ae52899961752cf2fd8edd0
Bskasan <b.kasan@yandex.com>
2023-01-24 11:28:46 +0200

    html css added

---
+++ checkout.js
@@ -0,0 +1,11 @@
+//const taxRate = 0.18;
+//const shippingPrice = 15.0;
+//const shippingFreePrice = 300;
+
+window.addEventListener("load", () => {
+  //localStorage.setItem("taxRate", taxRate);
+  //localStorage.setItem("shippingPrice", shippingPrice);
+  //localStorage.setItem("shippingFreePrice", shippingFreePrice);
+});
+
+
