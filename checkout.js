17e5e03ae62f23ae09860a8c10b329fffc58d75e
Bskasan <b.kasan@yandex.com>
2023-01-24 11:51:44 +0200

    calculate methods 1

--- checkout.js
+++ checkout.js
@@ -6,12 +6,14 @@ window.addEventListener("load", () => {
   //localStorage.setItem("taxRate", taxRate);
   //localStorage.setItem("shippingPrice", shippingPrice);
   //localStorage.setItem("shippingFreePrice", shippingFreePrice);
+
+  calculateCardPrice();
 });
 
 const productsDiv = document.querySelector(".products");
 
 //* Capturing ==> Static Parent ---> Childred
-//* We are trying to catch all click in one addEventListener by using Capturing Method 
+//* We are trying to catch all click in one addEventListener by using Capturing Method
 productsDiv.addEventListener("click", (e) => {
   //! e.target vs. e.currentTarget
   //! alert(e.target.tagName);
@@ -19,11 +21,24 @@ productsDiv.addEventListener("click", (e) => {
 
   if (e.target.className == "fa-solid fa-minus") {
     alert("minus clicked");
+    calculateProductPrice(e.target);
+    calculateCardPrice();
   } else if (e.target.classList.contains("fa-plus")) {
     alert("plus btn clicked");
+    calculateProductPrice(e.target);
+    calculateCardPrice();
   } else if (e.target.getAttribute("class") == "remove-product") {
     alert("remove btn clicked");
+    calculateCardPrice();
   } else {
     alert("other element clicked");
   }
 });
+
+const calculateProductPrice = (target) => {
+  //* Product total calculation
+};
+
+const calculateCardPrice = () => {
+  //* Cart total calculation from all products
+};
