e8d4ddb8e3056ee0802fd4c664c5958e92e02c66
Bskasan <b.kasan@yandex.com>
2023-01-24 11:41:49 +0200

    productsDiv

--- checkout.js
+++ checkout.js
@@ -8,4 +8,22 @@ window.addEventListener("load", () => {
   //localStorage.setItem("shippingFreePrice", shippingFreePrice);
 });
 
+const productsDiv = document.querySelector(".products");
 
+//* Capturing ==> Static Parent ---> Childred
+//* We are trying to catch all click in one addEventListener by using Capturing Method 
+productsDiv.addEventListener("click", (e) => {
+  //! e.target vs. e.currentTarget
+  //! alert(e.target.tagName);
+  //! alert(e.currentTarget.className);
+
+  if (e.target.className == "fa-solid fa-minus") {
+    alert("minus clicked");
+  } else if (e.target.classList.contains("fa-plus")) {
+    alert("plus btn clicked");
+  } else if (e.target.getAttribute("class") == "remove-product") {
+    alert("remove btn clicked");
+  } else {
+    alert("other element clicked");
+  }
+});
